b82459ff6b2f6fdb267e0eb3e11513a408515745
khacpv <khacpv@gmail.com>
2016-05-12 09:46:10 +0700

    update code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('It\'s work');
+  res.send('It works');
 });
 
 var HTTP_PORT = process.env.PORT || 3000;
